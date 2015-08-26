44edb746bcb846245e38f753aba2646d91fb0d7a
alexandra brown & Deron Johnson <student@epicodus.com>
2015-08-26 14:19:46 -0700

    Add failing tests for update and delete in Patron class

--- tests/PatronTest.php
+++ tests/PatronTest.php
@@ -108,6 +108,67 @@
             //Assert
             $this->assertEquals($test_patron2, $result);
         }
+
+        // test that update method is working for name
+        function test_updateName()
+        {
+            //Arrange
+            $name = "Suzie Palloozi";
+            $phone = "1-800-439-0398";
+            $test_patron = new Patron($name, $phone);
+            $test_patron->save();
+
+            $column_to_update = "name";
+            $new_info = "Suzie P";
+
+            //Act
+            $test_patron->update($column_to_update, $new_info);
+
+            //Assert
+            $result = Patron::getAll();
+            $this->assertEquals("Suzie P", $result[0]->getName());
+        }
+
+        // test that same update method is working for phone
+        function test_updatePhone()
+        {
+            //Arrange
+            $name = "Suzie Palloozi";
+            $phone = "1-800-439-0398";
+            $test_patron = new Patron($name, $phone);
+            $test_patron->save();
+
+            $column_to_update = "phone";
+            $new_info = "570-943-0483";
+
+            //Act
+            $test_patron->update($column_to_update, $new_info);
+
+            //Assert
+            $result = Patron::getAll();
+            $this->assertEquals("570-943-0483", $result[0]->getPhone());
+        }
+
+        function test_delete()
+        {
+            //Arrange
+            $name = "Suzie Palloozi";
+            $phone = "1-800-439-0398";
+            $test_patron = new Patron($name, $phone);
+            $test_patron->save();
+
+            $name2 = "Tac Zoltani";
+            $phone2 = "1-800-407-3930";
+            $test_patron2 = new Patron($name2, $phone2);
+            $test_patron2->save();
+
+            //Act
+            $test_patron->delete();
+
+            //Assert
+            $result = Patron::getAll();
+            $this->assertEquals($test_patron2, $result[0]);
+        }
     }
 
  ?>
