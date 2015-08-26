27f2829d9a7f214b8f9a7b4b80207890c5308cf2
alexandra brown & Deron Johnson <student@epicodus.com>
2015-08-26 10:42:34 -0700

    Add getTitle test for Book class

---
+++ tests/BookTest.php
@@ -0,0 +1,39 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    //require_once "src/Author.php";
+    require_once "src/Book.php";
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BookTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            //Author::deleteAll();
+            Book::deleteAll();
+        }
+
+        function test_getTitle()
+        {
+            //Arrange
+            $title = "Whimsical Fairytales, and other stories";
+            $genre = "Fantasy";
+            $test_book = new Book($title, $genre);
+
+            //Act
+            $result = $test_book->getTitle();
+
+            //Assert
+            $this->assertEquals($title, $result);
+
+        }
+    }
+
+ ?>
