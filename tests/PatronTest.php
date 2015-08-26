f791244e2bcba5f4e5e2372e671a7928d3186b18
alexandra brown & Deron Johnson <student@epicodus.com>
2015-08-26 13:58:24 -0700

    Add save, getAll and deleteAll tests for Patron class

--- tests/PatronTest.php
+++ tests/PatronTest.php
@@ -22,7 +22,7 @@
         function test_getName()
         {
             //Arrange
-            $name = "Suzie Pallozi";
+            $name = "Suzie Palloozi";
             $phone = "1-800-439-0398";
             $test_patron = new Patron($name, $phone);
 
@@ -35,7 +35,58 @@
 
         function test_save()
         {
+            //Arrange
+            $name = "Suzie Palloozi";
+            $phone = "1-800-439-0398";
+            $test_patron = new Patron($name, $phone);
 
+            //Act
+            $test_patron->save();
+
+            //Assert
+            $result = Patron::getAll();
+            $this->assertEquals($test_patron, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Suzie Palloozi";
+            $phone = "1-800-439-0398";
+            $test_patron = new Patron($name, $phone);
+            $test_patron->save();
+
+            $name2 = "Tac Zoltani";
+            $phone2 = "1-800-407-3930";
+            $test_patron2 = new Patron($name2, $phone2);
+            $test_patron2->save();
+
+            //Act
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals([$test_patron, $test_patron2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Suzie Palloozi";
+            $phone = "1-800-439-0398";
+            $test_patron = new Patron($name, $phone);
+            $test_patron->save();
+
+            $name2 = "Tac Zoltani";
+            $phone2 = "1-800-407-3930";
+            $test_patron2 = new Patron($name2, $phone2);
+            $test_patron2->save();
+
+            //Act
+            Patron::deleteAll();
+
+            //Assert
+            $result = Patron::getAll();
+            $this->assertEquals([], $result);
         }
     }
 
